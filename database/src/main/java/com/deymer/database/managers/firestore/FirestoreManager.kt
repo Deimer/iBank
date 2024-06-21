9c577e7c9ae0ebcd8d4b6b5241e046771d246a44
Deimer <deimer215@gmail.com>
2024-06-20 22:30:40 -0500

    New logic is implemented that will allow manipulating the data managed in Firestore.

--- database/src/main/java/com/deymer/database/managers/firestore/FirestoreManager.kt
+++ database/src/main/java/com/deymer/database/managers/firestore/FirestoreManager.kt
@@ -1,19 +1,70 @@
 package com.deymer.database.managers.firestore
 
+import com.deymer.database.DataBaseConstants.DEFAULT_STRING
+import com.deymer.database.DataBaseConstants.Names.KEY_NAME_DATABASE
+import com.deymer.database.DataBaseConstants.Tables.ACCOUNTS_TABLE
+import com.deymer.database.DataBaseConstants.Tables.TRANSACTIONS_TABLE
+import com.deymer.database.DataBaseConstants.Tables.USERS_TABLE
+import com.deymer.database.entities.AccountEntity
+import com.deymer.database.entities.TransactionEntity
+import com.deymer.database.entities.UserEntity
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
-class FirestoreManager: IFirestoreManager {
+class FirestoreManager @Inject constructor(
+    private val firestoreDatabase: FirebaseFirestore
+): IFirestoreManager {
 
-    override suspend fun <T : Any> save(entity: T): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun <T : Any> save(
+        entity: T
+    ): Boolean = suspendCoroutine { continuation ->
+        val tableName = getTableName(entity::class.java)
+        firestoreDatabase.collection(KEY_NAME_DATABASE)
+            .document(tableName)
+            .collection(tableName)
+            .document()
+            .set(entity)
+            .addOnCompleteListener { task ->
+                continuation.resume(task.isSuccessful)
+            }
     }
 
-    override suspend fun <T : Any> save(entity: T, entityId: String): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun <T : Any> save(
+        entity: T, entityId: String
+    ): Boolean = suspendCoroutine { continuation ->
+        val tableName = getTableName(entity::class.java)
+        firestoreDatabase.collection(KEY_NAME_DATABASE)
+            .document(tableName)
+            .collection(tableName)
+            .document(entityId)
+            .set(entity)
+            .addOnCompleteListener { task ->
+                continuation.resume(task.isSuccessful)
+            }
     }
 
-    override suspend fun <T : Any> save(entityList: List<T>): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun <T : Any> save(
+        entityList: List<T>
+    ): Boolean = coroutineScope {
+        val tasks = entityList.map { entity ->
+            async {
+                val table = getTableName(entity::class.java)
+                firestoreDatabase.collection(KEY_NAME_DATABASE)
+                    .document(table)
+                    .collection(table)
+                    .add(entity)
+                    .await()
+                true
+            }
+        }
+        tasks.awaitAll().all { it }
     }
 
     override suspend fun <T : Any> updateValueById(
@@ -21,8 +72,15 @@ class FirestoreManager: IFirestoreManager {
         keyField: String,
         valueField: T,
         table: String
-    ): Boolean {
-        TODO("Not yet implemented")
+    ): Boolean = suspendCoroutine { continuation ->
+        firestoreDatabase.collection(KEY_NAME_DATABASE)
+            .document(table)
+            .collection(table)
+            .document(documentId)
+            .update(keyField, valueField)
+            .addOnCompleteListener { task ->
+                continuation.resume(task.isSuccessful)
+            }
     }
 
     override suspend fun <T : Any> updateValue(
@@ -32,19 +90,53 @@ class FirestoreManager: IFirestoreManager {
         valueField: T,
         table: String
     ): Boolean {
-        TODO("Not yet implemented")
+        val documentSnapshot = firestoreDatabase.collection(KEY_NAME_DATABASE)
+            .document(table)
+            .collection(table)
+            .whereEqualTo(keyId, valueId)
+            .get()
+            .await()
+            .documents.firstOrNull()
+        return documentSnapshot?.let {
+            documentSnapshot.reference.update(keyField, valueField).isSuccessful
+        } ?: run { false }
     }
 
     override suspend fun <T : Any> fetchAll(entity: T): List<DocumentSnapshot> {
-        TODO("Not yet implemented")
+        val tableName = getTableName(entity::class.java)
+        return firestoreDatabase.collection(KEY_NAME_DATABASE)
+            .document(tableName)
+            .collection(tableName)
+            .get()
+            .await()
+            .documents
     }
 
-    override suspend fun getById(id: String, table: String): DocumentSnapshot? {
-        TODO("Not yet implemented")
+    override suspend fun getById(
+        id: String,
+        table: String
+    ): DocumentSnapshot? {
+        return firestoreDatabase
+            .collection(KEY_NAME_DATABASE)
+            .document(table)
+            .collection(table)
+            .document(id)
+            .get()
+            .await()
     }
 
-    override suspend fun getByValue(key: String, value: String, table: String): DocumentSnapshot? {
-        TODO("Not yet implemented")
+    override suspend fun getByValue(
+        key: String,
+        value: String,
+        table: String
+    ): DocumentSnapshot? {
+        return firestoreDatabase.collection(KEY_NAME_DATABASE)
+            .document(table)
+            .collection(table)
+            .whereEqualTo(key, value)
+            .get()
+            .await()
+            .documents.firstOrNull()
     }
 
     override suspend fun getListByValue(
@@ -52,6 +144,21 @@ class FirestoreManager: IFirestoreManager {
         value: String,
         table: String
     ): List<DocumentSnapshot> {
-        TODO("Not yet implemented")
+        return firestoreDatabase.collection(KEY_NAME_DATABASE)
+            .document(table)
+            .collection(table)
+            .whereEqualTo(key, value)
+            .get()
+            .await()
+            .documents
+    }
+
+    private fun <T> getTableName(value: Class<T>): String {
+        return when (value) {
+            UserEntity::class.java -> USERS_TABLE
+            AccountEntity::class.java -> ACCOUNTS_TABLE
+            TransactionEntity::class.java -> TRANSACTIONS_TABLE
+            else -> DEFAULT_STRING
+        }
     }
 }
